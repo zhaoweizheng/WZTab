1ad9118176b25b72a819be41a3d2660365dff300
赵伟争 <zhaoweizheng66@163.com>
2017-05-15 17:46:45 +0800

    创建应用

---
+++ WZTabBarViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZTabBarViewController
+//
+//  Created by zwz on 2017/5/15.
+//  Copyright © 2017年 赵伟争. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WZTabBarViewController/WZTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  WZTabBarController.h
+//  WZTabBarViewController
+//
+//  Created by zwz on 2017/5/15.
+//  Copyright © 2017年 赵伟争. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WZTabBarController : UITabBarController
+
+@end
